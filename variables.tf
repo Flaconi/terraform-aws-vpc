830c09cc3cf34a1124eedeb3d794a2b14b46ed45
waqas.yousaf <waqas.yousaf@flaconi.de>
2024-03-28 11:10:31 +0100

    OPS-0: Adding SG Rules As Var

--- variables.tf
+++ variables.tf
@@ -174,3 +174,15 @@ variable "manage_default_security_group" {
   type        = bool
   default     = true
 }
+
+variable "default_security_group_ingress" {
+  description = "List of maps of ingress rules to set on the default security group"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "default_security_group_egress" {
+  description = "List of maps of egress rules to set on the default security group"
+  type        = list(map(string))
+  default     = []
+}
